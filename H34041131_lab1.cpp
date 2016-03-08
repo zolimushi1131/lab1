5872cda134f5ba7174290f78b966e52b2188cf8a
zolimushi1131 <sophia6828@gmail.com>
2016-03-08 22:04:21 +0800

    add H34041131_lab1.cpp

---
+++ H34041131_lab1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int n;
+    cin>>n;
+    cout<<n<<"  ";
+    while(n!=1)
+    {   if(n%2==1)
+        {n=3*n+1;}
+        else
+        {n=n/2;}
+        cout<<n<<"  ";
+    }
+    return 0;
+}
